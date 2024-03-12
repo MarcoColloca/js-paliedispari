1acfe86ece6f390b0b723a40f99aecd3e41cd4c4
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-12 14:51:33 +0100

    Add → added basic JavaScript Logic for isPalindrome function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log('JS Trieal')
+
+
+//// dichiaro una funzione (isPalindrome) a cui assegno 1 parametri (word)
+
+
+
+
+
+
+
+
+function isPalindrome(word){
+
+    let wordLastIndex = word.length - 1
+    for(wordLetters = 0; wordLetters < word.length / 2; wordLetters++){
+        if(word[wordLetters] !== word[wordLastIndex]){
+            return false;
+        }
+        wordLastIndex--
+    }
+    return true
+}
+
+
+
+const wordCheck = isPalindrome('anna')
+
+console.log(wordCheck)
+
+
+
